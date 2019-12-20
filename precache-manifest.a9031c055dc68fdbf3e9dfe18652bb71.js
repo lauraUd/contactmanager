5e50fedca01d99315f31b088f5b3056453c21816
Laura Udrescu <Laura.Udrescu@endava.com>
2019-12-20 14:35:10 +0200

    Updates

---
+++ precache-manifest.a9031c055dc68fdbf3e9dfe18652bb71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ffcd096989354602f91447f09eda761",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "48f9f10e2d33fa6f2bfe",
+    "url": "/contactmanager/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "678326d4a0a40bbfa50b",
+    "url": "/contactmanager/static/css/main.6dba400b.chunk.css"
+  },
+  {
+    "revision": "48f9f10e2d33fa6f2bfe",
+    "url": "/contactmanager/static/js/2.65b50278.chunk.js"
+  },
+  {
+    "revision": "d7c02b5cfd2fce7b07312cd42a7244b2",
+    "url": "/contactmanager/static/js/2.65b50278.chunk.js.LICENSE"
+  },
+  {
+    "revision": "678326d4a0a40bbfa50b",
+    "url": "/contactmanager/static/js/main.5549afdb.chunk.js"
+  },
+  {
+    "revision": "2fcdbd17e9f9b8c99f1b",
+    "url": "/contactmanager/static/js/runtime-main.49f20f74.js"
+  }
+]);
